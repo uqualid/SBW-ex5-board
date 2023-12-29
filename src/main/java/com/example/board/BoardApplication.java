ff1a8d1aff7e61e7c083f5a7520b5d52ba36a0e2
uqual <uqualid@tukorea.ac.kr>
2023-12-30 00:46:34 +0900

    Initial commit

---
+++ src/main/java/com/example/board/BoardApplication.java
@@ -0,0 +1,17 @@
+package com.example.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+}
