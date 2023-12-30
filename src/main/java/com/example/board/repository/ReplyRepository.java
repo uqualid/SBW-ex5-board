ff8f78121ed60c8e6f86a2830946cbde92c75d30
uqual <uqualid@tukorea.ac.kr>
2023-12-30 23:10:28 +0900

    commit

--- src/main/java/com/example/board/repository/ReplyRepository.java
+++ src/main/java/com/example/board/repository/ReplyRepository.java
@@ -1,12 +1,20 @@
 package com.example.board.repository;
 
+import com.example.board.entity.Board;
 import com.example.board.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    //Board 삭제 시에 댓글 삭제
     @Modifying
     @Query("delete from Reply r where r.board.bno =:bno ")
     void deleteByBno(Long bno);
+
+    //게시물로 댓글 목록 가져오기
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
